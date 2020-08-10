fa5fa710b6d92a1c0257d58ce73e53339b8a7f6d
khoslaritwek <ritwekkhosla@gmail.com>
2020-08-10 12:36:03 +0530

    Some modifications

--- ChatBot.py
+++ ChatBot.py
@@ -3,17 +3,26 @@
 import speech_recognition as sr
 import time
 import re
+import numpy as np
+import pandas as pd
 
 def timeAndDate():
     return time.asctime( time.localtime(time.time()) )
 
+
 def IsTimeQuestion(text):
     if re.search(".*time.*|.*date.*|.*year.*",text):
         print ( "Bot :" + timeAndDate())
+        
 
 def IsAboutMe(text):
-    if re.search(".*who.*you|.*you.*|.*created.*|.*creatd.*|.*yourself.*", text):
-        print ("Bot : I am just a naive text bot preprogrammed to answer you stupid queries this is that way these things work dont they ? some  one with lots of time created me lol")
+    if re.search(".*who.*you|.*you.*|.*yourself.*", text):
+        print ("Bot : Just another chat bot answering your questions Duh!!")
+    if re.search(".*creator.*|.*created.*", text):
+        print ("Bot : Someone with alots of time created me ")
+    if re.search(".*sing.*|.*song.*", text):
+        print ("Bot: I am a baaad  guuuy Duh!!")
+        
 
 def Speech2Text():
     rec = sr.Recognizer()
@@ -27,6 +36,7 @@ def Speech2Text():
                     print(' Bot : Sorry i was not able to catch that,Speak again herhaps')
 
     return text
+
 
 
 
