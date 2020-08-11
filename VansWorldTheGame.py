ec5083ced11f3e598a3ac850609bceac04b908ef
khoslaritwek <ritwekkhosla@gmail.com>
2020-08-12 01:49:30 +0530

    nothing new here

--- VansWorldTheGame.py
+++ VansWorldTheGame.py
@@ -1,5 +1,33 @@
 import pygame as pg
+import os
+import numpy as np
+import random 
 
+
+def loadAssests():
+    assetList = os.listdir('assets-block')
+    assets = []
+
+    for mem in assetList:
+        path = os.path.join('assets-block', mem)
+        print(path)
+        img = pg.image.load(path)
+        assets.append(img)
+
+    return assets
+
+    
+def IntialSenceSetup(screen1):
+    assets =  loadAssests()
+
+    yPos = 0
+    while yPos < 160:
+        xPos = 0
+        while xPos < 1280 :
+            img = random.choice(assets)
+            screen1.blit(img, (xPos, yPos))
+            xPos = xPos + 80 
+        yPos = yPos + 20
     
 
 def PlayGame():
@@ -9,21 +37,21 @@ def PlayGame():
     screen = pg.display.toggle_fullscreen()
     icon = pg.image.load('VansLifeIcon.png')
     pg.display.set_icon(icon)
-    playerimg = pg.image.load('VAnsCarGoku.png')
-    playerX = 100
-    playerY = 100
-
+    IntialSenceSetup(screen1)
+    
     
     while True:
         closingFlag = False
         for event in pg.event.get():
+            print(event)
             if event.type == pg.QUIT:
                 pg.quit()
                 closingFlag = True
                 break
 
-            screen1.fill((25,178,255))
-            screen1.blit(playerimg, (playerX, playerY))
+            
+            
+            
             pg.display.update()
             
         if closingFlag == True:
